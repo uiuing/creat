2806adc21309b8f5dc3fb69dc03018075f9afbce
JavanTang <tzfjobmail@gmail.com>
2022-11-07 21:58:33 +0800

    add: Test websocket module

---
+++ service/tests/test_room.py
@@ -0,0 +1,152 @@
+import websockets
+import asyncio
+import json
+import send_data
+
+
+def pprint(obj):
+    return json.dumps(obj, indent=2, sort_keys=True, ensure_ascii=False)
+
+
+async def test_1():
+    """
+    测试1:
+    用户A 创建房间
+    用户B 加入房间
+    用户C 加入房间
+    用户A 加入房间
+    用户A 新建Node
+    用户B 新建Node
+
+    用户A 更新Nodes
+    用户B 删除Nodes
+    用户C 删除全部Nodes
+    用户D 覆盖全部Nodes
+    """
+    uri = "ws://localhost:8000/ws"
+
+    user_a = await websockets.connect(uri)
+    user_b = await websockets.connect(uri)
+
+    # 用户A 创建房间
+    print("用户A 创建房间")
+    print(f"用户A 发送信息> {pprint(send_data.create_meeting)}")
+    await user_a.send(json.dumps(send_data.create_meeting))
+    res = await user_a.recv()
+    res = json.loads(res)
+    print(f"用户A 接收信息> {pprint(res)}")
+    print('用户A 创建房间成功')
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+    # 用户B 加入房间
+    print("用户B 加入房间")
+    print(f"用户B 发送信息> {pprint(send_data.join_meeting_1)}")
+    await user_b.send(json.dumps(send_data.join_meeting_1))
+    res = await user_b.recv()
+    res = json.loads(res)
+    print(f"用户B 接收信息> {pprint(res)}")
+
+    res = await user_a.recv()
+    res = json.loads(res)
+    print(f"用户A 接收信息> {pprint(res)}")
+
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+    # 用户C 加入房间
+    print("用户C 加入房间")
+    user_c = await websockets.connect(uri)
+    print(f"用户C 发送信息> {pprint(send_data.join_meeting_2)}")
+    await user_c.send(json.dumps(send_data.join_meeting_2))
+    res = await user_c.recv()
+    res = json.loads(res)
+    print(f"用户C 接收信息> {pprint(res)}")
+
+    # 用户A 与 用户B 接受到用户C加入房间的消息
+    res = await user_a.recv()
+    res = json.loads(res)
+    print(f"用户A 接收信息> {pprint(res)}")
+
+    res = await user_b.recv()
+    res = json.loads(res)
+    print(f"用户B 接收信息> {pprint(res)}")
+
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+    # 用户A 新建Node
+    print("用户A 新建Node")
+    print(f"用户A 发送信息> {pprint(send_data.add_node_1)}")
+    await user_a.send(json.dumps(send_data.add_node_1))
+    
+    # 用户A 不需要收到消息
+    # res = await user_a.recv()
+    # res = json.loads(res)
+    # print(f"用户A 接收信息> {pprint(res)}")
+
+    # 用户B 用户C 接受到用户A新建Node的消息
+    res = await user_b.recv()
+    res = json.loads(res)
+    print(f"用户B 接收信息> {pprint(res)}")
+
+    res = await user_c.recv()
+    res = json.loads(res)
+    print(f"用户C 接收信息> {pprint(res)}")
+
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+    # 用户B 新建Node
+    print("用户B 新建Node")
+    print(f"用户B 发送信息> {pprint(send_data.add_node_2)}")
+    await user_b.send(json.dumps(send_data.add_node_2))
+
+    # 用户B 不需要收到消息
+    # res = await user_b.recv()
+    # res = json.loads(res)
+    # print(f"用户B 接收信息> {pprint(res)}")
+
+    # 用户A 用户C 接受到用户B新建Node的消息
+    res = await user_a.recv()
+    res = json.loads(res)
+    print(f"用户A 接收信息> {pprint(res)}")
+
+    res = await user_c.recv()
+    res = json.loads(res)
+    print(f"用户C 接收信息> {pprint(res)}")
+
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+    # 用户A 更新Nodes
+    print("用户A 更新Nodes")
+    print(f"用户A 发送信息> {pprint(send_data.nodes_update)}")
+    await user_a.send(json.dumps(send_data.nodes_update))
+
+    # 用户A 不需要收到消息
+    # res = await user_a.recv()
+    # res = json.loads(res)
+    # print(f"用户A 接收信息> {pprint(res)}")
+
+    # 用户B 用户C 接受到用户A更新Nodes的消息
+    res = await user_b.recv()
+    res = json.loads(res)
+    print(f"用户B 接收信息> {pprint(res)}")
+
+    res = await user_c.recv()
+    res = json.loads(res)
+    print(f"用户C 接收信息> {pprint(res)}")
+
+    print('----------------------------------------')
+    print('----------------------------------------')
+
+
+asyncio.get_event_loop().run_until_complete(test_1())
+
+
+# def room_1():
+#     asyncio.get_event_loop().run_until_complete(test_1())
+
+# if __name__ == "__main__":
+#     test_1()
