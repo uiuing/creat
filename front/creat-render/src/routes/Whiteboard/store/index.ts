830715db20f980b33cf17ae69ec3e8012cef5e86
uiuing <uiuing@foxmail.com>
2022-11-09 02:29:27 +0800

    fix: [creat-render] store key
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- front/creat-render/src/routes/Whiteboard/store/index.ts
+++ front/creat-render/src/routes/Whiteboard/store/index.ts
@@ -3,9 +3,15 @@ import { atom } from 'recoil'
 import { localforageEffect } from '../../../utils/data'
 
 export const userInfoState = atom({
-  key: 'whiteboardInfosState',
+  key: 'userInfoState',
   default: {},
   effects_UNSTABLE: [localforageEffect('creat-user-info')]
+})
+
+export const userTmpColorState = atom({
+  key: 'userTmpColorState',
+  default: {},
+  effects_UNSTABLE: [localforageEffect('creat-user-tmp-color')]
 })
 
 export const localDataState = atom({
