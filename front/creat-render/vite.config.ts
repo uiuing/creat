6adf0bd0aed5962ff58fe318e626761780004763
uiuing <uiuing@foxmail.com>
2022-11-13 04:27:31 +0800

    fix: [creat-render] build
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- front/creat-render/vite.config.ts
+++ front/creat-render/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    'process.env.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED': JSON.stringify(
+      process.env.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED
+    ),
+    'process.env.TERM': JSON.stringify(process.env.TERM),
+    'process.platform': JSON.stringify(process.platform)
   }
 })
