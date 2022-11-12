22bdc528dd85ea20109a16c69778771a592e8b6b
uiuing <uiuing@foxmail.com>
2022-11-13 04:27:31 +0800

    fix: [creat-render] build
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- front/creat-render/vite.config.ts
+++ front/creat-render/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    'process.env': { ...process.env, ...loadEnv('mode', process.cwd()) }
   }
 })
