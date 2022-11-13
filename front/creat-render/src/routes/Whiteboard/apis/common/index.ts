7adf7c6c364b0dcb750ffa0143e235a95ceaec54
uiuing <uiuing@foxmail.com>
2022-11-13 21:59:33 +0800

    fix: data sync diff
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- front/creat-render/src/routes/Whiteboard/apis/common/index.ts
+++ front/creat-render/src/routes/Whiteboard/apis/common/index.ts
@@ -1,5 +1,5 @@
-// export const httpsApiBaseUrl = 'https://api-creat.uiuing.com'
-// export const wssApiBaseUrl = 'wss://api-creat.uiuing.com'
+export const httpsApiBaseUrl = 'https://api-creat.uiuing.com'
+export const wssApiBaseUrl = 'wss://api-creat.uiuing.com'
 
-export const httpsApiBaseUrl = 'http://127.0.0.1:4911'
-export const wssApiBaseUrl = 'ws://127.0.0.1:4911'
+// export const httpsApiBaseUrl = 'http://127.0.0.1:4911'
+// export const wssApiBaseUrl = 'ws://127.0.0.1:4911'
