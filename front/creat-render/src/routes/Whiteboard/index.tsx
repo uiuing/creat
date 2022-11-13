1d798aa8032e4448631b76b15cf948456dac8a17
uiuing <uiuing@foxmail.com>
2022-11-13 22:45:18 +0800

    feat: [creat-render] fix dev website ✅
    Signed-off-by: uiuing <uiuing@foxmail.com>

--- front/creat-render/src/routes/Whiteboard/index.tsx
+++ front/creat-render/src/routes/Whiteboard/index.tsx
@@ -3,6 +3,10 @@ import styles from './style.module.scss'
 
 // 初始化白板ID信息
 window.whiteboardId = window.location.pathname.replaceAll('/', '')
+const h = window.location.host
+if (h !== 'creat.uiuing.com') {
+  window.location.replace(`https://creat.uiuing.com${window.location.pathname}`)
+}
 
 export default function Whiteboard() {
   return (
